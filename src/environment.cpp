2f48a45b382466768ae1da4a5e201978e11993aa
xiebian <xiebianwhu@gmail.com>
2022-05-27 15:11:02 +0800

    clustering obstacles cloud

--- src/environment.cpp
+++ src/environment.cpp
@@ -53,8 +53,19 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     // TODO:: Create point processor
     ProcessPointClouds<pcl::PointXYZ>* processor = new ProcessPointClouds<pcl::PointXYZ>();
     std::pair<pcl::PointCloud<pcl::PointXYZ>::Ptr, pcl::PointCloud<pcl::PointXYZ>::Ptr> segResult = processor->SegmentPlane(cloud, 100, 0.2);
-    renderPointCloud(viewer, segResult.first, "plane", Color(0,1,0));
-    renderPointCloud(viewer, segResult.second, "obs", Color(1,0,0));
+    //renderPointCloud(viewer, segResult.first, "plane", Color(0,1,0));
+    //renderPointCloud(viewer, segResult.second, "obs", Color(1,0,0));
+    std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> cloudClusters = processor->Clustering(segResult.second, 1., 3, 30 );
+    int clusterId = 0;
+    std::vector<Color> colors = {Color(1,0,0), Color(0,1,0), Color(0,0,1)};
+
+    for(pcl::PointCloud<pcl::PointXYZ>::Ptr cluster : cloudClusters)
+    {
+        std::cout << "cluster size ";
+        processor->numPoints(cluster);
+        renderPointCloud(viewer,cluster,"obstCloud"+std::to_string(clusterId),colors[clusterId]);
+        ++clusterId;
+    }
 }
 
 
