91644f057db14f08e7e31908bbcbf6b82ab842a6
xiebian <xiebianwhu@gmail.com>
2022-05-27 19:58:11 +0800

    implement kdtree

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -40,6 +40,28 @@ struct KdTree
 	{
 		// TODO: Fill in this function to insert a new point into the tree
 		// the function should create a new node and place correctly with in the root 
+		kd_insert_helper(&root, point, id, 0);
+	}
+
+	void kd_insert_helper(Node** n, std::vector<float> point, int id, int depth)
+	{
+		if(*n == NULL)
+		{
+			*n = new Node(point, id);
+			return;
+		}
+
+		int cd = depth%2;
+
+		if(point[cd] < (*n)->point[cd])
+		{
+			kd_insert_helper(&((*n)->left), point, id, depth+1);
+		}
+		else
+		{
+			kd_insert_helper(&((*n)->right), point, id, depth+1);
+		}
+
 
 	}
 
@@ -47,7 +69,41 @@ struct KdTree
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+		kd_search_helper(root, target, distanceTol,0, ids);
 		return ids;
+	}
+
+	void kd_search_helper(Node* n, std::vector<float> target, float distanceTol, int depth,std::vector<int>& ids)
+	{
+		if(n == NULL)
+		{
+			return;
+		}
+
+		if ( (n->point[0] <= target[0] + distanceTol && n->point[0] >= target[0] - distanceTol) && 
+			 (n->point[1] <= target[1] + distanceTol && n->point[1] >= target[1] - distanceTol) &&
+			 (sqrt((n->point[0]-target[0])*(n->point[0]-target[0]) + (n->point[1]-target[1])*(n->point[1]-target[1])) < distanceTol)
+			)
+		{
+			ids.push_back(n->id);
+		}
+
+		int cd = depth % 2;
+
+		if(n->point[cd] <= target[cd] + distanceTol && n->point[cd] >= target[cd] - distanceTol)
+		{
+			kd_search_helper(n->left, target, distanceTol, depth+1, ids);
+			kd_search_helper(n->right, target, distanceTol, depth+1, ids);
+		}
+		else if(n->point[cd] > target[cd] + distanceTol)
+		{
+			kd_search_helper(n->left, target, distanceTol, depth+1, ids);
+		}
+		else if(n->point[cd] < target[cd] - distanceTol)
+		{
+			kd_search_helper(n->right, target, distanceTol, depth+1, ids);
+		}
+
 	}
 	
 
