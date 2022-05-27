9f84c51e4ec99bee5d30cdd7b5afee79802ecfcb
xiebian <xiebianwhu@gmail.com>
2022-05-27 10:52:34 +0800

    render point cloud from lidar without scene

--- src/environment.cpp
+++ src/environment.cpp
@@ -42,13 +42,14 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     // ----------------------------------------------------
     
     // RENDER OPTIONS
-    bool renderScene = true;
+    bool renderScene = false;
     std::vector<Car> cars = initHighway(renderScene, viewer);
     
     // TODO:: Create lidar sensor 
     Lidar *lidar = new Lidar(cars, 0);
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = lidar->scan();
-    renderRays(viewer, lidar->position, cloud);
+    //renderRays(viewer, lidar->position, cloud);
+    renderPointCloud(viewer, cloud, "cloud", Color(255,255,255));
     // TODO:: Create point processor
   
 }
